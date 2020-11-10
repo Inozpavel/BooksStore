44aec5e3fe08bcd04df0825ad886daffd67e29f6
Inozpavel <Inozpavel@mail.ru>
2020-11-10 16:51:13 +0300

    Добавлена страница для входа в аккаунт

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BooksStore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BooksStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View();
+        [HttpGet]
+        public ViewResult Index() => View();
+
+        [HttpGet]
+        public ViewResult Login() => View();
     }
 }
