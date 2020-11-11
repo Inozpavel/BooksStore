8ed881ec0a27a79d6abbf49cfdbd6abeb40d8b2f
Inozpavel <Inozpavel@mail.ru>
2020-11-11 20:09:09 +0300

    Добавлена модель Book

---
+++ Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BooksStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="У книги должно быть название!")]
+        [DisplayName("Название книги")]
+        public string Name { get; set; }
+        
+        public Author Author { get; set; }
+        
+        public Category Category { get; set; }
+
+        [DisplayName("Описание книги")]
+        public string Description { get; set; }
+
+        public Book()
+        {
+
+        }
+    }
+}
