73c3a89a33048cc811c9dd5a91ae70a37facbe78
Inozpavel <Inozpavel@mail.ru>
2020-11-11 20:10:01 +0300

    Добавлена модель Author

---
+++ Models/Author.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BooksStore.Models
+{
+    public class Author
+    {
+        [Key]
+        [DisplayName("Имя автора")]
+        [Required(ErrorMessage = "У автора должно быть имя!")]
+        public string Name { get; set; }
+
+        [DisplayName("Описание автора")]
+        public string Description { get; set; }
+
+        public Author() : this("", null)
+        {
+        }
+
+        public Author(string name) => Name = name;
+
+        public Author(string name, string description) : this(name) => Description = description;
+
+        public override string ToString() => Name;
+    }
+}
