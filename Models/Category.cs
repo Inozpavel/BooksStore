a566db8b89ddc0b5f44ac6389f25a8fe483f662f
Inozpavel <Inozpavel@mail.ru>
2020-11-11 20:10:35 +0300

    Добавлена модель Category

---
+++ Models/Category.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BooksStore.Models
+{
+    public class Category
+    {
+        [Key]
+        [DisplayName("Категория")]
+        [Required(ErrorMessage ="У категории должно быть название!")]
+        public string Name { get; set; }
+
+        [DisplayName("Описание категории")]
+        public string Description { get; set; }
+
+        public Category() : this("", null)
+        {
+        }
+
+        public Category(string name) => Name = name;
+
+        public Category(string name, string description) : this(name) => Description = description;
+
+        public override string ToString() => Name;
+    }
+}
