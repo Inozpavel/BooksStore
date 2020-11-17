e3cbc761fa0dec267e0de7b4d00ee60aa28306c6
Inozpavel <Inozpavel@mail.ru>
2020-11-17 21:17:06 +0300

    Добавлена проверка совпадения пароля на серверной стороне

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BooksStore.Models
 {
@@ -29,6 +30,14 @@ namespace BooksStore.Models
         [Required(ErrorMessage = "Значение не может быть пустым!")]
         [MinLength(8, ErrorMessage = "Пароль не может быть меньше 8 символов")]
         [DisplayName("Пароль")]
+        [Compare("PasswordConfirmation", ErrorMessage = "Пароли должны совпадать!")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Значение не может быть пустым!")]
+        [MinLength(8, ErrorMessage = "Пароль не может быть меньше 8 символов")]
+        [DisplayName("Подтверждение пароля")]
+        [Compare("Password", ErrorMessage ="Пароли должны совпадать!")]
+        [NotMapped]
+        public string PasswordConfirmation { get; set; }
     }
 }
