3d43b318bd6036b7688ca459dc51e88087e8b2a5
Inozpavel <Inozpavel@mail.ru>
2020-12-20 18:03:20 +0300

    Для книги добавлена стоимость, написана миграция

--- Models/Book.cs
+++ Models/Book.cs
@@ -12,6 +12,9 @@ namespace BooksStore.Models
         [DisplayName("Название книги")]
         public string Name { get; set; }
 
+        [DisplayName("Цена товара")]
+        public decimal Price { get; set; }
+
         public int AuthorId { get; set; }
 
         public Author Author { get; set; }
